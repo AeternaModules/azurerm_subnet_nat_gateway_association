abc91513459b3ebd9956e84cb7fc3bcb5e80649e
Daniel Allen <dan@aeternagroup.net>
2026-01-11 19:34:39 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "subnet_nat_gateway_associations" {
+  description = "All subnet_nat_gateway_association resources"
+  value       = azurerm_subnet_nat_gateway_association.subnet_nat_gateway_associations
+}
+
