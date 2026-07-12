fd1d76f243ace4e9e770ec6ae521b05e41bfa7f0
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:32:15 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "subnet_nat_gateway_associations_id" {
+  description = "Map of id values across all subnet_nat_gateway_associations, keyed the same as var.subnet_nat_gateway_associations"
+  value       = { for k, v in azurerm_subnet_nat_gateway_association.subnet_nat_gateway_associations : k => v.id }
+}
 output "subnet_nat_gateway_associations_nat_gateway_id" {
   description = "Map of nat_gateway_id values across all subnet_nat_gateway_associations, keyed the same as var.subnet_nat_gateway_associations"
   value       = { for k, v in azurerm_subnet_nat_gateway_association.subnet_nat_gateway_associations : k => v.nat_gateway_id }
