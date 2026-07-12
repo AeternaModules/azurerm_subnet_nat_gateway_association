63e38650c7a324cd68b4a29ab2dc3730d55bfbbe
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:21:01 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,9 @@
-output "subnet_nat_gateway_associations" {
-  description = "All subnet_nat_gateway_association resources"
-  value       = azurerm_subnet_nat_gateway_association.subnet_nat_gateway_associations
-}
 output "subnet_nat_gateway_associations_nat_gateway_id" {
-  description = "List of nat_gateway_id values across all subnet_nat_gateway_associations"
-  value       = [for k, v in azurerm_subnet_nat_gateway_association.subnet_nat_gateway_associations : v.nat_gateway_id]
+  description = "Map of nat_gateway_id values across all subnet_nat_gateway_associations, keyed the same as var.subnet_nat_gateway_associations"
+  value       = { for k, v in azurerm_subnet_nat_gateway_association.subnet_nat_gateway_associations : k => v.nat_gateway_id }
 }
 output "subnet_nat_gateway_associations_subnet_id" {
-  description = "List of subnet_id values across all subnet_nat_gateway_associations"
-  value       = [for k, v in azurerm_subnet_nat_gateway_association.subnet_nat_gateway_associations : v.subnet_id]
+  description = "Map of subnet_id values across all subnet_nat_gateway_associations, keyed the same as var.subnet_nat_gateway_associations"
+  value       = { for k, v in azurerm_subnet_nat_gateway_association.subnet_nat_gateway_associations : k => v.subnet_id }
 }
 
