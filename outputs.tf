c06ad38e8bff6f38699b56648f0af9074016cbc6
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:51:32 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
 output "subnet_nat_gateway_associations_id" {
   description = "Map of id values across all subnet_nat_gateway_associations, keyed the same as var.subnet_nat_gateway_associations"
-  value       = { for k, v in azurerm_subnet_nat_gateway_association.subnet_nat_gateway_associations : k => v.id }
+  value       = { for k, v in azurerm_subnet_nat_gateway_association.subnet_nat_gateway_associations : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "subnet_nat_gateway_associations_nat_gateway_id" {
   description = "Map of nat_gateway_id values across all subnet_nat_gateway_associations, keyed the same as var.subnet_nat_gateway_associations"
-  value       = { for k, v in azurerm_subnet_nat_gateway_association.subnet_nat_gateway_associations : k => v.nat_gateway_id }
+  value       = { for k, v in azurerm_subnet_nat_gateway_association.subnet_nat_gateway_associations : k => v.nat_gateway_id if v.nat_gateway_id != null && length(v.nat_gateway_id) > 0 }
 }
 output "subnet_nat_gateway_associations_subnet_id" {
   description = "Map of subnet_id values across all subnet_nat_gateway_associations, keyed the same as var.subnet_nat_gateway_associations"
-  value       = { for k, v in azurerm_subnet_nat_gateway_association.subnet_nat_gateway_associations : k => v.subnet_id }
+  value       = { for k, v in azurerm_subnet_nat_gateway_association.subnet_nat_gateway_associations : k => v.subnet_id if v.subnet_id != null && length(v.subnet_id) > 0 }
 }
 
